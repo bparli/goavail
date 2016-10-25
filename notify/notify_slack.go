65f3541af492c490b74c4969e443215e03161fca
Ben Parli <ben.parli@gonitro.com>
2016-10-24 17:47:16 -0700

    add Slack integration and dry-run mode via cmd line

---
+++ notify/notify_slack.go
@@ -0,0 +1,47 @@
+package notify
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+type slackNotifications struct {
+	UseSlack  bool
+	slackAddr string
+}
+
+var SlackNotify slackNotifications
+
+type Params map[string]string
+
+func InitSlack(slackAddr string) {
+	if slackAddr != "" {
+		SlackNotify.UseSlack = true
+		SlackNotify.slackAddr = slackAddr
+	}
+}
+
+func (s *slackNotifications) SendToSlack(ipAddr string, domain string, addOrRemove string, dryRun bool) {
+	log.Debugln("Dry Run is True, sending Slack notification", "https://hooks.slack.com/services/"+s.slackAddr)
+	text := ""
+	if dryRun {
+		text = "Dry Run set to True.  Would have " + addOrRemove + " " + ipAddr + " and configured entries from/to domain " + domain + " "
+	} else {
+		text = "Dry Run set to False. " + addOrRemove + " " + ipAddr + " and configured entries from/to domain " + domain + " "
+	}
+	var p Params = map[string]string{
+		"text": text,
+	}
+	json, _ := json.Marshal(p)
+	resp, _ := http.PostForm("https://hooks.slack.com/services/"+s.slackAddr, url.Values{"payload": []string{string(json)}})
+	if resp.StatusCode != http.StatusOK {
+		body, _ := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		log.Errorf("status code: %d, response body: %s", resp.StatusCode, body)
+	}
+
+}
